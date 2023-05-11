1ad23f396e2913c2eff76efceb1eb6c5ab0f6678
gost-d <103152567+gost-d@users.noreply.github.com>
2023-05-12 00:57:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -79,6 +79,37 @@ print("PRECEDESSOR: " + str(precedessor(108,8)))
 
 
 
+/////////////////////////////////////
+
+///////////////////////////////////
+
+
+def successor(n, k, t):
+    
+    #Tutaj sprawdzamy, czy jest różnica większa niż 1 między elementami. Jeśli tak - robimy +1 na danym elemencie. I reszta zostje tak samo. 
+
+    for i in range (0, k):
+        if ( t[k-i-1] + 1 < t[k-i-2] ):
+            t[k-i-1] = t[k-i-1] + 1
+            return t
+    #Jeśli nie było takie różnicy, to znaczy że musimy podbić pierwszą cyferkę, bo mamy układy w stylu 432, 543 itp. - czyli żeby to zwiększyć, to rzeba rzejść wyżej
+    #przy peirwszej cyferce. Dodajemy 1 do pierwszej, a potem jeszcze od tyłu ustawiamy wartości od 1 do tej cyferki, np. 621 --> od tyłu, czyli 1 na koniec, potem 2, no a potem 6
+
+    # if ( t[0] < n ):
+    #     t[0] = t[0] + 1
+    #     for i in range ( 1, k ):
+    #         t[k-i] = i
+    #     return t
+    #
+    t[0] = t[0] + 1
+    for i in range ( 1, k ):
+        t[k-i] = i
+         
+
+    return t
+
+
+
 
 
 
